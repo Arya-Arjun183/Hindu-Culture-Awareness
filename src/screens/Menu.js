8b17861d90f48e9170fb2579c137ad1674b692e4
punit.arya <punit.arya@servicenow.com>
2024-10-08 22:41:36 -0700

    Fix google analytics

--- src/screens/Menu.js
+++ src/screens/Menu.js
@@ -11,7 +11,7 @@ import styles from './Styles';
 import { logPageView } from '../analytics';
 
 const handleClick = (props, menu) => {
-  logPageView('AppHome', props);
+  logPageView('AppHome', menu);
 
   props.navigation.navigate(menu);
 };
